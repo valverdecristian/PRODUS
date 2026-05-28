f18416ecb010c80b715a7366fa9700274404dd01
valverdecristian <crizthian2010@gmail.com>
2026-05-28 15:28:31 -0300

    feat: tarea clase 9 - expandir catalogo y equipo en firebase

--- src/components/contactos/TarjetaContacto.jsx
+++ src/components/contactos/TarjetaContacto.jsx
@@ -1,13 +1,14 @@
 import styles from './TarjetaContacto.module.css';
 
-const TarjetaContacto = ({ nombre, email, puesto, foto }) => {
+const TarjetaContacto = ({ nombre, linkedin, rol, foto }) => {
     return (
         <div className={styles.item}>
             <h2>{nombre}</h2>
-            <p>{email}</p>
-            <p>{puesto}</p>
+            <p>{rol}</p>
             <img src={foto} alt={nombre} width="200" />
-
+            <a href={linkedin} target="_blank" rel="noopener noreferrer" className={styles.linkedinLink}>
+                Ver LinkedIn
+            </a>
         </div>
     );
 };
